a2ff266945515036a063bde39abfd5f4bb867532
Pierre Narvor <pnarvor@gmail.com>
2018-05-21 15:56:44 +0200

    Initial commit

---
+++ env.sh
@@ -0,0 +1,8 @@
+# Environment setup for PhD folder
+
+# Get the directory of the current script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export PHD_DIR=$DIR
+export PHD_DATA_DIR=$PHD_DIR/Data
+
